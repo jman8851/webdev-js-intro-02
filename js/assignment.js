1e02bfcc63069ecf1423ea15af3ea8dbc4213d76
Jason Caldwell <jman8851@gmail.com>
2024-12-19 15:14:10 -0500

    placed an X on finished assignments. Worked with some experimental code and commented out once done. Did the js assignment and updated functions as directed.

--- js/assignment.js
+++ js/assignment.js
@@ -8,23 +8,36 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = "Be a Better Coder";
+const currentYear = 2025;
+let willMeetResolution = true;
 
 function updateYear() {
     // write the logic
+    yearElement.innerText = currentYear;
 
 }
 
 function updateResolution() {
-    // Update this function 
+    // Update this function
+    resolutionElement.innerText = resolution; 
 
+}
+
+function updatewillMeetResolution() {
+    willMeetResolutionElement.innerText = willMeetResolution ? "Yes" : "No";
 }
 
 function render() {
     // Finish writing this function
+    updateYear();
+    updateResolution();
+    updatewillMeetResolution();
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
+    render();
 
-})
+});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,38 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+const myName = "Jason";
+console.log(myName)             // Output: "Jason"
+console.log(typeof myName)      // Output: "string"
 
+const one = 1;
+console.log(one)                // Output: 1
+console.log(typeof one);        // Output: "number"
+
+let isMentor = true;            
+console.log(isMentor)           // Output: true
+console.log(typeof isMentor)    // Output: "boolean"
+
+isMentor = false;
+
+console.log(isMentor)           // Output: false
+console.log(typeof isMentor)    // Output: "boolean"
+*/
+/*
+
+let mood = "happy";
+console.log(mood);
+console.log(typeof mood);
+
+mood = "excited";
+console.log(mood);
+
+mood = 42;  // changing data type to number
+console.log(mood);
+console.log(typeof mood);
+
+mood = true;  // changing data type to boolean
+console.log(mood);
+console.log(typeof mood);
+*/
